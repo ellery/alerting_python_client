dc94440633f914c3e8c649756683a74c43f61d5c
Ellery J. Durgin <ellery@civitium.com>
2016-06-25 23:29:23 -0400

    alerter working

--- alerter.py
+++ alerter.py
@@ -4,11 +4,88 @@ import json
 from array import array
 from struct import pack
 from sys import byteorder
-import copy
+from pprint import pprint
+import urllib
+import getopt, sys
 
+try:
+  opts, args = getopt.getopt(sys.argv[1:], "fg:v", ["file=", "group="])
+except getopt.GetoptError as err:
+  # print help information and exit:
+  print str(err)  # will print something like "option -a not recognized"
+  usage()
+  sys.exit(2)
+output = None
+verbose = False
+for o, a in opts:
+  if o == "-v":
+    verbose = True
+  elif o in ("-f", "--file"):
+    source_file = a
+  elif o in ("-g", "--group"):
+    group = a
+  else:
+    assert False, "unhandled option"
 
+with open('config.json') as config_file:    
+    config = json.load(config_file)
 
+try:
+  t = group
+except:
+  print "No Group ID Passed"
 
-url = 'http://ES_search_demo.com/document/record/_search?pretty=true'
-data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
-response = requests.get(url, data=data)
+try:
+  t = source_file
+except:
+  print "No File passed"
+  
+
+  
+try:
+  t = config['hostname']
+except:
+    print "unable to load valid config file. Missing Host Name"
+    exit(1)   
+    
+try: 
+  t = config['api_endpoint']
+except:
+    print "unable to load valid config file. Missing API End Point"
+    exit(1)  
+try:
+  t=config['key']
+except:
+    print "unable to load valid config file. Missing Application Key"
+    exit(1)  
+        
+print source_file      
+try:
+  with open(source_file, "rb") as audio_file:
+      encoded_audio_file = base64.b64encode(audio_file.read())  
+except:
+  print "unable to load Audio File"
+  exit(1)
+    
+
+url = config['hostname'] + config['api_endpoint']
+
+payload = dict()
+payload['message'] = dict()
+payload['message']['group_id'] = group
+payload['message']['audio_file'] = encoded_audio_file
+
+try:
+  response = requests.post(url, json=payload)
+except: 
+  print "Unable to reach Server"
+  exit(1)
+  
+  
+if response.status_code == requests.codes.ok:
+  print "Valid response"
+  exit(0)
+else:
+  print "Error in response: " + str(response.status_code)
+  exit(1)
+  
